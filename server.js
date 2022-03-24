e2e76c45e5b39b83c083b39273f515b1f619df22
MohamedAshraf-SP <Mohamed.AshrafSP@gmail.com>
2022-03-24 18:54:04 +0200

    api and server added

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
+port = process.env.PORT || 3000
+
 
 // Require Express to run server and routes
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-Parser')
 
 // Start up an instance of app
 
+const app = express()
+
+
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // Cors for cross origin allowance
-
+app.use(cors())
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+app.listen(port, () => {
+    console.log(`server is runing on port :${port}`)
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,30 @@
 /* Global Variables */
 
+const apiKey = "45020210bbd6bb4e6353d10d9636b814"
+
+
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear()
+
+
+//event on submit
+
+const generate = document.getElementById("generate")
+
+
+//fetch data function
+
+const getData = async () => {
+    const zip = document.getElementById("zip")
+    const feeling = document.getElementById("feelings")
+    apiURL = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apiKey}`
+
+
+    const res = await fetch(apiURL)
+    console.log(res)
+
+
+}
+
+generate.addEventListener('click', getData)
